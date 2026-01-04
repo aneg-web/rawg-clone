f34c731453012491378745c20ebaca58e3f6d4c5
Anton Egorushkin <egorushkin.anton267@gmail.com>
2026-01-04 04:16:20 +0100

    Filter games by genre

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
+import { type Genre } from "./hooks/useGenres";
 import { Grid, GridItem } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 
 function App() {
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
   return (
     <Grid
       templateAreas={{
@@ -19,10 +22,10 @@ function App() {
         <Navbar />
       </GridItem>
       <GridItem hideBelow="lg" area="aside" paddingX={5}>
-        <GenreList />
+        <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} />
       </GridItem>
       <GridItem area="main">
-        <GameGrid />
+        <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
   );
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,13 @@
-import { HStack, Image, List, Spinner, Text } from "@chakra-ui/react";
+import { type Genre } from "../hooks/useGenres";
+import { Button, HStack, Image, List, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -17,7 +22,13 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              variant="plain"
+              fontSize="lg"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </List.Item>
       ))}
