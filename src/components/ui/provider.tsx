443f2c984b0a9fbad2a853d7ee4ffed06aef886a
Anton Egorushkin <egorushkin.anton267@gmail.com>
2026-01-04 22:19:30 +0100

    Customizing theme

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,15 +1,16 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { system } from "../../theme";
+import { ColorModeProvider } from "./color-mode";
 
-export function Provider(props: ColorModeProviderProps) {
+export function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
+    <ChakraProvider value={system}>
+      <ColorModeProvider defaultTheme="dark" enableSystem={false}>
+        {children}
+      </ColorModeProvider>
     </ChakraProvider>
-  )
+  );
 }
