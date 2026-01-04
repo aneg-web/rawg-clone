30c493a8db48224b42906c9c94097e30d3a42fc3
Anton Egorushkin <egorushkin.anton267@gmail.com>
2026-01-04 03:37:28 +0100

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ const GameGrid = () => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        gap="10px"
+        gap="3px"
         padding="10px"
       >
         {isLoading &&
