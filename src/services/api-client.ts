5532749683aba26faaec79f8c01e8dd0924f22ba
Anton Egorushkin <egorushkin.anton267@gmail.com>
2026-04-02 17:01:06 +0200

    Refactor fetch games with React Query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-  baseURL: 'https://api.rawg.io/api',
+  baseURL: "https://api.rawg.io/api",
   params: {
-    key: '969064c1bf314cbdb73d98779117a20e'
-  }
-})
+    key: "969064c1bf314cbdb73d98779117a20e",
+  },
+});
