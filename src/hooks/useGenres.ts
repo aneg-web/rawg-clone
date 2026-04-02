5189b1562805b288cebb060cab0a0eee0a80920b
Anton Egorushkin <egorushkin.anton267@gmail.com>
2026-04-02 18:16:04 +0200

    Implement infinite scroll

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
-import genres from "../data/genres";
 
 const apiClient = new APIClient<Genre>("/genres");
 
@@ -15,7 +14,6 @@ const useGenres = () =>
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24h
-    //initialData: { count: genres.length, results: genres },
   });
 
 export default useGenres;
