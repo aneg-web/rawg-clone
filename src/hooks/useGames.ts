db65c3d23c8cba098041b1f9cfd1e795031838fa
Anton Egorushkin <egorushkin.anton267@gmail.com>
2026-04-02 19:23:52 +0200

    Simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import type { GameQuery } from "../App";
 import APIClient, { type FetchResponse } from "../services/api-client";
 import type { Platform } from "./usePlatforms";
+import ms from "ms";
 
 const apiClient = new APIClient<Game>("/games");
 
@@ -31,7 +32,7 @@ const useGames = (gameQuery: GameQuery) =>
       return lastPage.next ? allPages.length + 1 : undefined;
     },
     initialPageParam: 1,
-    staleTime: 24 * 60 * 60 * 1000, // 24h
+    staleTime: ms("24h"),
   });
 
 export default useGames;
